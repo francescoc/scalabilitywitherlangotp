14dcb1a5a67926cce3e3c31e2e4719068ec02b2a
francescoc <francesco@erlang-solutions.com>
2016-01-06 12:54:54 +0000

    gen fsm examples

---
+++ ch6/otp/coffee_fsm_timeout.erl
@@ -0,0 +1,85 @@
+-module(coffee_fsm_timeout).
+-behaviour(gen_fsm).
+
+-export([start_link/0, stop/0]).
+-export([init/1, terminate/3, handle_event/3]).    %% Callback functions
+-export([selection/2, payment/2, remove/2]).       %% States
+-export([americano/0, cappuccino/0, tea/0, espresso/0, pay/1, cancel/0, cup_removed/0]). %% Client Functions 
+
+-define(TIMEOUT, 10000).
+
+start_link() ->
+    gen_fsm:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) -> 
+    hw:reboot(),
+    hw:display("Make Your Selection", []),
+    process_flag(trap_exit, true),
+    {ok, selection, []}.
+
+%% Client Functions for Drink Selections
+tea()        -> gen_fsm:send_event(?MODULE,{selection,tea,100}).   
+espresso()   -> gen_fsm:send_event(?MODULE,{selection,espresso,150}).
+americano()  -> gen_fsm:send_event(?MODULE,{selection,americano,150}).   
+cappuccino() -> gen_fsm:send_event(?MODULE,{selection,cappuccino,150}).
+
+%% Client Functions for Actions
+pay(Coin)     -> gen_fsm:send_event(?MODULE,{pay, Coin}).
+cancel()      -> gen_fsm:send_event(?MODULE,cancel).
+cup_removed() -> gen_fsm:send_event(?MODULE,cup_removed).
+
+%% State: drink selection
+selection({selection,Type,Price}, _LoopData) ->
+  hw:display("Please pay:~w",[Price]),
+  {next_state, payment, {Type, Price, 0}, ?TIMEOUT};
+selection({pay, Coin}, LoopData) ->
+  hw:return_change(Coin),
+  {next_state, selection, LoopData};
+selection(_Other, LoopData) ->  
+  {next_state, selection, LoopData}.
+
+
+payment({pay, Coin}, {Type,Price,Paid}) when Coin+Paid >= Price ->
+    NewPaid = Coin + Paid,
+    hw:display("Preparing Drink.",[]),
+    hw:return_change(NewPaid - Price),
+    hw:drop_cup(), hw:prepare(Type),
+    hw:display("Remove Drink.", []),
+    {next_state, remove, []};
+payment({pay, Coin}, {Type,Price,Paid}) when Coin+Paid < Price ->
+    NewPaid = Coin + Paid,
+    hw:display("Please pay:~w",[Price - NewPaid]),
+    {next_state, payment, {Type, Price, NewPaid}, ?TIMEOUT};
+payment(cancel, {_Type, _Price, Paid}) ->
+    hw:display("Make Your Selection", []),
+    hw:return_change(Paid),
+    {next_state, selection, []};
+payment(timeout, {_Type, _Price, Paid}) ->
+  hw:display("Make Your Selection", []),
+  hw:return_change(Paid),
+  {next_state, selection, []};
+payment(_Other, LoopData) ->
+    {next_state, payment, LoopData, ?TIMEOUT}.
+
+
+%% State: remove cup 
+remove(cup_removed, LoopData) ->
+    hw:display("Make Your Selection", []),
+    {next_state, selection, LoopData};
+remove({pay, Coin}, LoopData) ->
+    hw:return_change(Coin),
+    {next_state, remove, LoopData};
+remove(_Other, LoopData) ->          
+    {next_state, remove, LoopData}.
+
+
+stop() -> gen_fsm:sync_send_all_state_event(?MODULE, stop).
+
+handle_event(stop, _State, LoopData) ->
+    {stop, normal, LoopData}.
+
+terminate(_Reason, payment, {_Type,_Price,Paid}) ->
+    hw:return_change(Paid);
+terminate(_Reason, _StateName, _LoopData) ->
+    ok.
+
---
+++ ch6/otp/test_fsm.erl
@@ -0,0 +1,52 @@
+%%% @copyright (C) 2013, Francesco Cesarini
+
+-module(test_fsm).
+-behaviour(gen_fsm).
+
+%% API
+-export([start_link/2, start/2]).
+
+%% gen_fsm callbacks
+-export([init/1, state_name/2, state_name/3, handle_event/3,
+	 handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {}).
+
+start_link(TimerMs, Options) ->
+    gen_fsm:start_link(?MODULE, TimerMs, Options).
+start(TimerMs, Options) ->
+    gen_fsm:start(?MODULE, TimerMs, Options).
+
+init(0) ->
+    {stop, stopped};
+init(1) ->
+    {next_state, selection, []};
+init(TimerMs) ->
+    timer:sleep(TimerMs),
+    ignore.
+
+state_name(_Event, State) ->
+    {next_state, state_name, State}.
+
+state_name(_Event, _From, State) ->
+    Reply = ok,
+    {reply, Reply, state_name, State}.
+
+handle_event(_Event, StateName, State) ->
+    {next_state, StateName, State}.
+
+handle_sync_event(_Event, _From, StateName, State) ->
+    Reply = ok,
+    {reply, Reply, StateName, State}.
+
+handle_info(_Info, StateName, State) ->
+    {next_state, StateName, State}.
+
+terminate(_Reason, _StateName, _State) ->
+    ok.
+
+code_change(_OldVsn, StateName, State, _Extra) ->
+    {ok, StateName, State}.
+
